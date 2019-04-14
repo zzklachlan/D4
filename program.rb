cc87bc564b1bca36ce14764afae60c144d1a7f6b
zzklachlan <zzklanchlan@gmail.com>
2019-04-14 15:49:00 -0400

    f

--- program.rb
+++ program.rb
@@ -55,7 +55,37 @@ class Program
   def check_prev_hash(prev_hash, curr_hash, b_num)  
     unless prev_hash.eql? curr_hash
       puts "Line #{b_num}: Previous has was #{curr_hash}, should be #{prev_hash}"
-      puts "BLOCKCHAIN INVALID"
+      puts 'BLOCKCHAIN INVALID'
+      exit(0)
+    end
+  end
+
+  def check_timestamp(prev_time, curr_time, b_num)
+    prev_time1 = prev_time.split('.')[0]
+    prev_time2 = prev_time.split('.')[1]
+    curr_time1 = curr_time.split('.')[0]
+    curr_time2 = curr_time.split('.')[1]
+    unless prev_time1.to_i == curr_time1.to_i && prev_time2.to_i < curr_time2.to_i
+      puts "Line #{b_num}: Previous timestamp #{prev_time} => new timestamp #{curr_time}"
+      puts 'BLOCKCHAIN INVALID'
+      exit(0)
+    end
+  end
+
+  def check_balance(b_num)
+    invalid_addr = ''
+    invalid_balance = 0
+    @users.each do |key, value|
+      if value < 0
+        invalid_addr = key 
+        invalid_balance = value 
+        break
+      end
+    end
+
+    unless invalid_addr == ''
+      puts "Line #{b_num}: address #{invalid_addr} has #{invalid_balance} billcoins!"
+      puts 'BLOCKCHAIN INVALID'
       exit(0)
     end
   end
@@ -64,14 +94,17 @@ class Program
   def run
     count = 0
     prev_hash = ''
+    prev_timestamp = ''
     curr_block = []
     @file.each do |line|
       @blocks << line.chomp # each line is a block
       curr_block = @blocks[count].split('|')      
       check_block_number(count, curr_block[0])
-
+      check_timestamp(prev_timestamp, curr_block[3], curr_block[0].to_i) unless count.zero?
       check_prev_hash(prev_hash, curr_block[1], curr_block[0].to_i) unless count.zero?
       transaction(curr_block[2])
+      check_balance(curr_block[0].to_i)
+      prev_timestamp = curr_block[3]
       prev_hash = curr_block[4]
       count += 1
     end
