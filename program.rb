ca9b6bbbcf1532d9fd0cf63eb71348960d3a78f6
Jay <jayhope125@gmail.com>
2019-04-14 23:16:13 -0400

    after rubocop check

--- program.rb
+++ program.rb
@@ -24,7 +24,7 @@ class Program
     begin
       file = File.open(file_name, 'r')
     rescue StandardError
-      puts "Usage: ruby verifier.rb <name_of_file> \nname_of_file = name of file to verify"
+      puts "Usage: ruby verifier.rb <name_of_file> \n    name_of_file = name of file to verify"
       return false
     end
     file
@@ -46,6 +46,7 @@ class Program
       single_tran = x.split(/>|[()]/)
       error_code = check_invalid_format(single_tran[0], single_tran[1], single_tran[2].to_i, b_num, tran)
       return error_code if error_code == 5
+
       modify_user(single_tran[0], single_tran[1], single_tran[2].to_i)
     end
     0
@@ -55,47 +56,48 @@ class Program
     string.scan(/\D/).empty?
   end
 
-  def check_invalid_format(from_addr, to_addr, amount, b_num, tran)
+  def check_invalid_format(from_addr, to_addr, amount, _b_num, _tran)
     return 5 if from_addr.nil? || to_addr.nil? || amount.nil?
 
     is_valid = true
     is_valid = false unless from_addr.length == 6 && to_addr.length == 6
-    is_valid = false if amount < 0
+    is_valid = false if amount.negative?
     is_valid = false if check_string(from_addr) == false && from_addr != 'SYSTEM'
     is_valid = false if check_string(to_addr) == false && to_addr != 'SYSTEM'
 
     return 5 if is_valid == false
+
     0
   end
 
   # This method checks the block number
   def check_block_number(count, b_num)
-    return 2 unless count == b_num.to_i 
+    return 2 unless count == b_num.to_i
+
     0
   end
 
-  def check_prev_hash(prev_hash, curr_hash, b_num)  
+  def check_prev_hash(prev_hash, curr_hash, _b_num)
     return 4 unless prev_hash.eql? curr_hash
+
     0
   end
 
-  def check_timestamp(prev_time, curr_time, b_num)
+  def check_timestamp(prev_time, curr_time, _b_num)
     prev_time1 = prev_time.split('.')[0].to_i
     prev_time2 = prev_time.split('.')[1].to_i
     curr_time1 = curr_time.split('.')[0].to_i
     curr_time2 = curr_time.split('.')[1].to_i
 
     return 3 if prev_time1 > curr_time1 || (prev_time1 == curr_time1 && prev_time2 > curr_time2)
+
     0
   end
 
-  def check_balance(b_num)
-    invalid_addr = ''
-    invalid_balance = 0
+  def check_balance
     @users.each do |key, value|
-      return ['6', key, value] if value < 0
+      return ['6', key, value] if value.negative?
     end
-
     0
   end
 
@@ -104,7 +106,7 @@ class Program
     sum = 0
     x_val = 0
     string_to_hash.unpack('U*').each do |x|
-      unless @hash_val.has_key?(x)
+      if !@hash_val.key?(x)
         x_val = ((x**3000) + (x**x) - (3**x)) * (7**x)
         @hash_val[x] = x_val
       else
@@ -112,14 +114,15 @@ class Program
       end
       sum += x_val
     end
-    sum = (sum % 65536).to_s(16)
-    
+    sum = (sum % 65_536).to_s(16)
     return ['7', string_to_hash, expected_hash, sum] unless expected_hash == sum
+
     0
   end
 
-  def check_extra_pipe(b_num, block)
+  def check_extra_pipe(block)
     return 1 unless block.length == 5
+
     0
   end
 
@@ -132,34 +135,48 @@ class Program
     @file.each do |line|
       @blocks << line.chomp # each line is a block
       curr_block = @blocks[count].split('|')
-      @error_code = check_extra_pipe(count, curr_block)
+      @error_code = check_extra_pipe(curr_block)
       return "Line #{count}: extra pipe found! \nBLOCKCHAIN INVALID" if error_code == 1
-      
+
       @error_code = check_block_number(count, curr_block[0])
-      return "Line #{count}: Invalid block number #{curr_block[0]}, should be #{count} \nBLOCKCHAIN INVALID" if error_code == 2
-      
+      if error_code == 2
+        return "Line #{count}: Invalid block number #{curr_block[0]}, should be #{count} \nBLOCKCHAIN INVALID"
+      end
+
       @error_code = check_timestamp(prev_timestamp, curr_block[3], curr_block[0].to_i) unless count.zero?
-      return "Line #{count}: Previous timestamp #{prev_timestamp} => new timestamp #{curr_block[3]} \nBLOCKCHAIN INVALID" if error_code == 3
-      
+      if error_code == 3
+        return "Line #{count}: Previous timestamp #{prev_timestamp} => "\
+        "new timestamp #{curr_block[3]} \nBLOCKCHAIN INVALID"
+      end
+
       @error_code = check_prev_hash(prev_hash, curr_block[1], curr_block[0].to_i) unless count.zero?
-      return "Line #{count}: Previous has was #{curr_block[1]}, should be #{prev_hash}\nBLOCKCHAIN INVALID" if error_code == 4
-      
+      if error_code == 4
+        return "Line #{count}: Previous has was #{curr_block[1]}, should be #{prev_hash}\nBLOCKCHAIN INVALID"
+      end
+
       @error_code = transaction(curr_block[2], curr_block[0])
-      return "Line #{count}: Could not parse transactions list '#{curr_block[2]}' \nBLOCKCHAIN INVALID" if error_code == 5
-      
-      error_balance = check_balance(curr_block[0].to_i)
-      @error_code = error_balance[0].to_i unless error_balance == 0
-      return "Line #{count}: address #{error_balance[1]} has #{error_balance[2]} billcoins! \nBLOCKCHAIN INVALID" if error_code == 6
-      
+      if error_code == 5
+        return "Line #{count}: Could not parse transactions list '#{curr_block[2]}' \nBLOCKCHAIN INVALID"
+      end
+
+      error_balance = check_balance
+      @error_code = error_balance[0].to_i unless error_balance.zero?
+      if error_code == 6
+        return "Line #{count}: address #{error_balance[1]} has #{error_balance[2]} billcoins! \nBLOCKCHAIN INVALID"
+      end
+
       error_hash = check_hash(curr_block[0], curr_block[1], curr_block[2], curr_block[3], curr_block[4])
-      @error_code = error_hash[0].to_i unless error_hash == 0
-      return "Line #{count}: String '#{error_hash[1]}' hash set to #{error_hash[2]}, should be #{error_hash[3]}\nBLOCKCHAIN INVALID" if error_code == 7
+      @error_code = error_hash[0].to_i unless error_hash.zero?
+      if error_code == 7
+        return "Line #{count}: String '#{error_hash[1]}' hash set to #{error_hash[2]},
+         should be #{error_hash[3]}\nBLOCKCHAIN INVALID"
+      end
 
       prev_timestamp = curr_block[3]
       prev_hash = curr_block[4]
       count += 1
     end
-    output  
+    output
     error_code
   end
 
--- verifier.rb
+++ verifier.rb
@@ -4,13 +4,13 @@ require 'flamegraph'
 require_relative './program.rb'
 
 Flamegraph.generate('flame_graph.html') do
-	if (ARGV.count != 1)
-		 puts "Usage: number of arguments has to be one!"
-		 exit(0)
-	end
-	file = Program.open_file(ARGV[0])
-	exit(0) if file == false
-	program = Program.new(file)
-	error = program.run
-	puts error unless error.is_a? Integer
+  if ARGV.count > 1
+    puts 'Usage: number of arguments has to be one!'
+    exit(0)
+  end
+  file = Program.open_file(ARGV[0])
+  exit(0) if file == false
+  program = Program.new(file)
+  error = program.run
+  puts error unless error.is_a? Integer
 end
